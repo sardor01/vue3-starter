f23c6318b1da2a01b6f3c12125834389cce6d52e
sardor01 <sardor.astanov.01@mail.ru>
2023-10-26 12:27:09 +0500

    chore: deps updates

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <p>
+    <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">Vitesse Lite</a>
+  </p>
+  <p>
+    <em class="text-sm opacity-75">Opinionated Vue and Vite Starter Template</em>
+  </p>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,5 @@
 <template>
   <main class="px-4 py-10 text-center font-sans text-gray-700">
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">Vitesse Lite</a>
-    </p>
-    <p>
-      <em class="text-sm opacity-75">Opinionated Vue and Vite Starter Template</em>
-    </p>
+    <test-component />
   </main>
 </template>
