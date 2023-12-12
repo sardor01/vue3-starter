bc0101b49836d70871b6a07c4405c12b1e620ae2
sardor01 <sardor.astanov.01@mail.ru>
2023-12-12 15:52:11 +0500

    chore: deps updates

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="px-4 py-10 text-center font-sans text-gray-700">
+  <main class="px-4 py-10 text-center text-gray-700 font-sans">
     <test-component />
   </main>
 </template>
